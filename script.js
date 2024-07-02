88322ba6decf91927db3eb2e10878ee78635a072
Gabriel Tsei <92765565+BielTsei@users.noreply.github.com>
2024-07-02 13:55:13 -0300

    Adicionado imagens, contato e menu mobile

--- script.js
+++ script.js
@@ -8,8 +8,20 @@ class MobileNavbar {
     this.handleClick = this.handleClick.bind(this);
   }
 
+  animationLinks() {
+    this.navLinks.forEach((link, index) =>
+      link.style.animation
+        ? (link.style.animation = " ")
+        : (link.style.animation = `navLinkFade 0.5s ease forwards ${
+            index / 7 + 0.3
+          }s`)
+    );
+  }
+
   handleClick() {
     this.navList.classList.toggle(this.activeClass);
+    this.mobileMenu.classList.toggle(this.activeClass);
+    this.animationLinks();
   }
 
   addClickEvent() {
